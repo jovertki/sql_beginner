d45079fcd0494d1f099f5665163f0095694f055f
Jovertki <wa9head@gmail.com>
2022-03-30 22:19:13 +0300

    day06ex06

--- day06/ex06/day06_ex06.sql
+++ day06/ex06/day06_ex06.sql
@@ -1,1 +1,11 @@
-CREATE SEQUEN
+CREATE SEQUENCE seq_person_discounts
+	INCREMENT 1;
+
+SELECT setval('seq_person_discounts', (SELECT MAX(id) FROM person_discounts));
+
+ALTER TABLE person_discounts
+ALTER COLUMN id set DEFAULT nextval('seq_person_discounts');
+
+--for testing
+INSERT INTO person_discounts(person_id, pizzeria_id, discount)
+VALUES(1,2, 50);
